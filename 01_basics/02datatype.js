14445af7546e39421fbba03eab9e62d16639a467
Aashish Parmar <146443752+AashishParmar73@users.noreply.github.com>
2023-10-31 15:43:18 +0000

    datatype

---
+++ 01_basics/02datatype.js
@@ -0,0 +1,19 @@
+"use strict" // treat all js ode newer version 
+
+// alert("hello"); we are using node.js, not browser
+
+let name = "John";
+let age = 24;
+let isLoggedId = false;
+
+// number => 2 to power 53
+// bigint 
+// string => '' ""
+// boolean => true/false
+// null => standalone value ka typeOf =>object
+// undefined =>
+//symbol=>unique
+
+// object
+
+console.log(typeof "john");
