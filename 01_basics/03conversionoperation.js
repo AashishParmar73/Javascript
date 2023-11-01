bf867dd1ade2bcc4746f9c46ecb78956a4350f78
Aashish Parmar <146443752+AashishParmar73@users.noreply.github.com>
2023-11-01 13:00:31 +0000

    conversion of data type

--- 01_basics/03conversionoperation.js
+++ 01_basics/03conversionoperation.js
@@ -26,3 +26,89 @@ let someNumber = 33;
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+// ********************** Operations *******************
+
+let value = 3;
+let negValue = -value;
+console.log(negValue);
+
+// JavaScript Operators
+// Operators are used to assign values, compare values, perform arithmetic operations, and more.
+
+// There are different types of JavaScript operators:
+
+// Arithmetic Operators
+// Assignment Operators
+// Comparison Operators
+// Logical Operators
+// Conditional Operators
+// Type Operators
+
+// 01 . JavaScript Arithmetic Operators
+// Arithmetic operators are used to perform arithmetic between variables and/or values.
+// Given that y = 5, the table below explains the arithmetic operators:
+
+var y = 5;
+var x = y + 2;
+console.log(x);
+
+var y = 5;
+var x = y - 2;
+console.log(x);
+
+var y = 5;
+var x = y * 2;
+console.log(x);
+
+var y = 5;
+var x = y / 2;
+console.log(x);
+
+var y = 5;
+var x = y ** 2;
+console.log(x);
+
+var y = 5;
+var x = y % 2;
+console.log(x);
+
+/* var y = 5;
+var x = y ++ 2;
+console.log(x);
+
+var y = 5;
+var x = y -- 2;
+console.log(x);
+ */
+
+let str1 = "john";
+let str2 = "hello";
+let str3 = str1 + str2;
+console.log(str3)
+// string add 
+
+console.log("1" + 2);
+console.log(2 + "1");
+console.log("1" + 2 + 2);
+// first value string and string he consader karta hai 
+console.log(2 + 2 + "1")
+// first value add to add to string he karta hai 
+
+console.log((3 + 2) * 5 % 2);
+console.log(+true);
+console.log(+"");
+
+
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter);
+
+console.log(2 + true)
+// 2 +  true=> 1  = 3 output
+
+
+
+
+
+
