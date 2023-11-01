320e0f3af0a7d92846d7389560944da959a42506
Aashish Parmar <146443752+AashishParmar73@users.noreply.github.com>
2023-11-01 14:39:29 +0000

    Comparison Operators

---
+++ 01_basics/04comparison.js
@@ -0,0 +1,17 @@
+/* JavaScript Operators
+Operators are used to assign values, compare values, perform arithmetic operations, and more.
+
+There are different types of JavaScript operators:
+
+Arithmetic Operators
+Assignment Operators
+Comparison Operators
+Logical Operators
+Conditional Operators
+Type Operators */
+
+/* 01. Comparison Operators
+Comparison operators are used in logical statements to determine equality or difference between variables or values.
+
+Given that x = 5, the table below explains the comparison operators: */
+
