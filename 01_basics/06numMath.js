129439abf94f509cd88e78f96ea3a5b5bde65d15
Aashish Parmar <146443752+AashishParmar73@users.noreply.github.com>
2023-11-03 07:06:42 +0000

    numberMath

---
+++ 01_basics/06numMath.js
@@ -0,0 +1,35 @@
+/* const score = 400;
+console.log(score);
+
+const balance = new Number(100);
+console.log(balance);
+console.log(balance.toString().length);
+// numbaer to string to convert
+
+console.log(balance.toFixed(5));
+
+const otherNumber = 23.8965;
+
+console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000;
+console.log (hundreds.toLocaleString("en-IN")); */
+
+// ++++++++++++++++++++++ Maths +++++++++++++++++++
+
+console.log(Math);
+
+console.log(Math.abs(-8));
+console.log(Math.round(4.6));
+console.log(Math.ceil(2.2));
+console.log(Math.floor(2.2));
+console.log(Math.min(7, 5, 8, 9, 6, 4, 1, 2, 3));
+console.log(Math.max(1, 5, 4, 7, 6, 9, 7, 3));
+
+console.log(Math.random());
+console.log(Math.random() * 10 + 1);
+console.log(Math.floor(Math.random() * 10 + 1));
+
+let min = 10;
+let max = 20;
+console.log(Math.floor(Math.random() * (max - min + 1) + min));
