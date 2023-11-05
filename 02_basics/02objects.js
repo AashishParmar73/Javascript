0d128c8efcdf356e412fa12f52276df0524aee1a
Aashish Parmar <146443752+AashishParmar73@users.noreply.github.com>
2023-11-05 03:44:55 +0000

    Objects Methods

--- 02_basics/02objects.js
+++ 02_basics/02objects.js
@@ -35,3 +35,91 @@ User.greetingTwo = function(){
 
 console.log(User.greeting())
 console.log(User.greetingTwo())
+
+// ************************************************////////////////////////
+
+// ye ek singleton hai / {} object hai
+// const tinder = new Object();
+
+// ye ek singleton object nahi hai / {} object hai
+const tinderUser = {};
+
+tinderUser.id = "123abc";
+tinderUser.name = "Sunny";
+tinderUser.isLoggedIn = false;
+console.log(tinderUser);
+
+const regularUser = {
+  email: "some@gmail.com",
+  fullName: {
+    userfullName: {
+      firstname: "John",
+      lastname: "Cena",
+    },
+  },
+};
+
+// console.log(regularUser.fullName.userfullName);
+
+const obj1 = {
+  1: "a",
+  2: "2",
+};
+const obj2 = {
+  3: "c",
+  4: "d",
+};
+
+// const obj3 = { obj1, obj2 };
+// assign() object return karta hai
+// const obj3 = Object.assign(obj1,obj2)
+// console.log(obj3);
+const obj3 = { ...obj1, ...obj2 };
+console.log(obj3 === obj1);
+
+// Array of object
+const User = [
+  {
+    id: 1,
+    email: "h@gmail.com",
+  },
+  {
+    id: 2,
+    email: "h@gmail.com",
+  },
+  {
+    id: 3,
+    email: "h@gmail.com",
+  },
+  {
+    id: 4,
+    email: "h@gmail.com",
+  },
+];
+console.log(Object.values(User));
+// console.log(Object.entries(User))
+
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+
+const course = {
+    coursename: "Js in Hindi",
+    price: "999",
+    courseInstructor: "John",
+}
+
+// const {courseInstructor} = course;
+const {courseInstructor: objectpara, price} = course
+console.log(objectpara,price) 
+
+
+// JSON stands for JavaScript Object Notation
+// JSON is a lightweight data interchange format
+// JSON is language independent *
+// JSON is "self-describing" and easy to understand
+// * The JSON syntax is derived from JavaScript object notation syntax, but the JSON format is text only. Code for reading and generating JSON data can be written in any programming language.
+
+/* {
+    "name": "The Rock",
+    "coursename" : "JS in Hindi",
+} */
